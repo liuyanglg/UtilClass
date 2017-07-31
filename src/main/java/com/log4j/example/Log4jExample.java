a35e94e0cfea809c49682b66a81d0e62806d488b
liuyanglg <liuyanglg@outlook.com>
2017-07-31 10:52:10 +0800

    Log4j配置学习

--- src/main/java/com/log4j/example/Log4jExample.java
+++ src/main/java/com/log4j/example/Log4jExample.java
@@ -1,24 +1,54 @@
 package com.log4j.example;
 
 import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 
-import javax.annotation.Resources;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.Enumeration;
 import java.util.Properties;
 
 public class Log4jExample {
-    public static final String PROPERTY_FILE = "properties/log4j.properties";
+    public static final String PROPERTY_FILE = "/properties/log4j.properties";
     public static Logger log = Logger.getLogger(Log4jExample.class.getName());
 
-    public static void initLog4j(){
-        InputStream is=null;
+    public void initLog4j() {
+        InputStream is = null;
         Properties pro = new Properties();
-        is = 
+        try {
+//            is = new FileInputStream(PROPERTY_FILE);
+            is = this.getClass().getResourceAsStream(PROPERTY_FILE);
+            pro.load(is);
+            Enumeration keys = pro.keys();
+            while (keys.hasMoreElements()) {
+                String key = (String) keys.nextElement();
+//                System.out.println(key + "：" + pro.getProperty(key));
+            }
+            PropertyConfigurator.configure(pro);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (is != null) {
+                    is.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
 
     public static void main(String[] args) {
+        Log4jExample log4jExample = new Log4jExample();
+        log4jExample.initLog4j();
+
         log.debug("debug message");
         log.info("info message");
+        log.error("error message");
+        log.error("error message");
     }
 }
