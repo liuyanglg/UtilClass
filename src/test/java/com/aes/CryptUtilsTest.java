dac0af28907f2e304afe3110a94fd4a2f2ce71db
liuyanglg <liuyanglg@outlook.com>
2017-08-29 16:54:50 +0800

    1.删除编译产生的文件

--- src/test/java/com/aes/CryptUtilsTest.java
+++ src/test/java/com/aes/CryptUtilsTest.java
@@ -11,7 +11,7 @@ import java.io.File;
 public class CryptUtilsTest {
     @Test
     public void encryptFile() throws Exception {
-        String fp = "E:\\Config\\work\\jdbc3\\jdbc.properties";
+        String fp = "D:\\Document\\Jdbc\\jdbc.local.properties";
 //        Scanner sc = new Scanner(System.in);
 //        System.out.println("Input file path: ");
 //        String inputPath=sc.next();
@@ -29,7 +29,7 @@ public class CryptUtilsTest {
 
     @Test
     public void decryptFile() throws Exception {
-        String fp = "E:\\Config\\work\\jdbc3\\jdbc.properties";
+        String fp = "D:\\Document\\Jdbc\\jdbc.properties";
 //        Scanner sc = new Scanner(System.in);
 //        System.out.println("Input file path: ");
 //        String inputPath=sc.next();
